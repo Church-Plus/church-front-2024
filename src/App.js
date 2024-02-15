5b64cc9541ff3cb31920fb15cab2a6448f657d57
YearaChoi <22200750@handong.ac.kr>
2024-02-15 21:30:35 +0900

    feat: 라우트에 first main page 경로 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import FolderPage from "./pages/HomePage/FolderPage";
 import MonthPage from "./pages/HomePage/MonthPage";
+import FirstMainPage from "./pages/HomePage/FirstMainPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<FirstMainPage />}></Route>
         <Route path="/MonthPage" element={<MonthPage />}></Route>
         <Route path="/MonthPage/:month" element={<MonthPage />}></Route>
         <Route
