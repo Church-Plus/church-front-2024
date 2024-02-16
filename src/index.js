f65c74676d941f61df4f8a0ef3c969a01930247c
YearaChoi <22200750@handong.ac.kr>
2024-02-16 16:52:18 +0900

    chore: fonts 폴더 경로 변경

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import GlobalStyle from "./fonts/GlobalStyle";
+import GlobalStyle from "./assets/fonts/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
