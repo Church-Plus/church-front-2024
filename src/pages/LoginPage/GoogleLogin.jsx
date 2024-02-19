770d79bd43bc2e4e43e78a09fda1dbca04e74a01
YearaChoi <22200750@handong.ac.kr>
2024-02-19 19:55:00 +0900

    style: 로그인페이지 스타일 변경

--- src/pages/LoginPage/GoogleLogin.jsx
+++ src/pages/LoginPage/GoogleLogin.jsx
@@ -1,10 +1,15 @@
 import React from "react";
-import Logo from "../../assets/Logos/C+_LoginLogo.svg";
+import Logo from "../../assets/Logos/loginLogo1.svg";
 import googleIcons from "../../assets/Icons/google.svg";
 import styled from "styled-components";
+import background from "../../assets/bgImg/LoginBgImg.png";
 
 const Wrapper = styled.div`
   text-align: center;
+  height: 100vh;
+  background: url(${background});
+  background-size: cover;
+  background-repeat: no-repeat;
 `;
 
 const ChurchPlusLogo = styled.div`
@@ -14,19 +19,26 @@ const ChurchPlusLogo = styled.div`
   }
 `;
 
+const Description = styled.div`
+  padding-top: 0.5rem;
+  font-size: 29px;
+`;
+
 const LoginBtn = styled.button`
-  margin-top: 12rem;
-  font-size: 35px;
-  width: 500px;
-  height: 80px;
+  margin-top: 13rem;
+  font-size: 30px;
+  font-weight: 100;
+  width: 415px;
+  height: 70px;
   border: none;
-  background-color: #e8e8ef;
+  border-radius: 38px;
+  background-color: rgba(232, 232, 239, 0.5);
   text-align: left;
 
   cursor: pointer;
   img {
-    height: 40px;
-    padding: 0rem 1.5rem;
+    height: 35px;
+    padding: 0rem 0.8rem;
   }
 
   img,
@@ -50,6 +62,11 @@ const GoogleLogin = () => {
       <ChurchPlusLogo>
         <img src={Logo} alt="Church+ 아이콘" />
       </ChurchPlusLogo>
+
+      <Description>
+        주차별 악보 관리부터 콘티 생성까지 <br></br>
+        Church+로 손쉽게 해결하세요!
+      </Description>
 
       <LoginBtn onClick={handleLogin}>
         <img src={googleIcons} alt="구글 로고" />
