99fb7c1e5241c83fe3271350db3a0fc82869cbd6
YearaChoi <22200750@handong.ac.kr>
2024-02-23 01:39:08 +0900

    feat: 악보 페이지 더미이미지 생성

--- src/pages/HomePage/FolderPage.jsx
+++ src/pages/HomePage/FolderPage.jsx
@@ -12,10 +12,46 @@ import {
 import { useParams } from "react-router-dom";
 import UploadModal from "../../components/Modal/UploadModal";
 import SwitchToggle from "../../components/Common/SwitchToggle";
+import styled from "styled-components";
+
+const FolderItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 3rem;
+  padding-left: 5.4rem;
+`;
+
+const FolderImage = styled.img`
+  height: 12.6rem;
+  width: 19.3rem;
+  object-fit: cover;
+  border-radius: 30px;
+  cursor: pointer;
+
+  filter: opacity(0.7) drop-shadow(0 0 0 #2e0b70);
+
+  &:hover {
+    filter: opacity(0.8) drop-shadow(0 0 0 #2e0084);
+  }
+`;
+
+const ImageContainer = styled.div`
+  position: relative;
+  width: fit-content;
+`;
 
 function FolderPage() {
   const params = useParams();
   const folderName = params.content;
+
+  const songData = [
+    {
+      title: "Song 1",
+      img: "https://mblogthumb-phinf.pstatic.net/20160524_274/lordship46_1464073042426hr2zD_JPEG/%C7%CF%B3%AA%B4%D4%C0%C7_%BC%BC%B0%E8%28%C2%FC_%BE%C6%B8%A7%B4%D9%BF%EE_%B0%F7%C0%CC%B6%F3%29-001-001.jpg?type=w800",
+    },
+  ];
+
   return (
     <>
       <Header />
@@ -39,6 +75,14 @@ function FolderPage() {
           <Box>
             <FolderContainer>
               <UploadModal />
+              {songData.map((song, index) => (
+                <FolderItem key={index}>
+                  <ImageContainer>
+                    <FolderImage src={song.img} alt={song.title} />
+                  </ImageContainer>
+                  <p>{song.title}</p>
+                </FolderItem>
+              ))}
             </FolderContainer>
           </Box>
         </Wrapper>
