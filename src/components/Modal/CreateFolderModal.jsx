6bce11f7212e4994f16a475acf9240b888b3c309
YearaChoi <22200750@handong.ac.kr>
2024-02-24 06:29:32 +0900

    style: 컴포넌트 폰트 적용, 커서 포인터 왼쪽정렬

--- src/components/Modal/CreateFolderModal.jsx
+++ src/components/Modal/CreateFolderModal.jsx
@@ -47,10 +47,11 @@ const InputFolderName = styled.input`
   border: none;
   outline: none;
   background-color: #e8e8ef;
-  text-align: center;
+  /* text-align: center; */
   font-size: 24px;
   font-family: "Pretendard";
   &::placeholder {
+    text-align: center;
     color: #bbbbbb;
   }
 `;
